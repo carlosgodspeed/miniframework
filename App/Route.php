8fdb0c506619c9f49d9726b8e0b34b5e7729a499
carlosgodspeed <carlosminibics@gmail.com>
2023-08-01 13:22:10 -0300

    controllers

--- App/Route.php
+++ App/Route.php
@@ -1,27 +1,58 @@
 <?php
 
-    namespace App;
+namespace App;
 
-    class Route {
+class Route {
 
-        public function initRoutes() {
+	private $routes;
 
-            $routes['Home'] = array(
-                'route' => '/',
-                'controller' => 'indexController',
-                'action' => 'index'
-            );
+	public function __construct() {
+		$this->initRoutes();
+		$this->run($this->getUrl());
+	}
 
-            $routes['Sobre_nos'] = array(
-                'route' => '/sobre_nos',
-                'controller' => 'indexController',
-                'action' => 'sobreNos'
-            );
-        }
+	public function getRoutes() {
+		return $this->routes;
+	}
 
-        public function getUrl() {
-            return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH );
-        }
-    }
+	public function setRoutes(array $routes) {
+		$this->routes = $routes;
+	}
+
+	public function initRoutes() {
+
+		$routes['home'] = array(
+			'route' => '/',
+			'controller' => 'indexController',
+			'action' => 'index'
+		);
+
+		$routes['sobre_nos'] = array(
+			'route' => '/sobre_nos',
+			'controller' => 'indexController',
+			'action' => 'sobreNos'
+		);
+
+		$this->setRoutes($routes);
+	}
+
+	public function run($url) {
+		foreach ($this->getRoutes() as $key => $route) {
+			if($url == $route['route']) {
+				$class = "App\\Controllers\\".ucfirst($route['controller']);
+
+				$controller = new $class;
+				
+				$action = $route['action'];
+
+				$controller->$action();
+			}
+		}
+	}
+
+	public function getUrl() {
+		return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+	}
+}
 
 ?>
