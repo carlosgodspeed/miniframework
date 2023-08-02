690b2dbcdcdc898fd5abb1f73a99eb8975064993
carlosgodspeed <carlosminibics@gmail.com>
2023-08-02 11:44:07 -0300

    definindo um layout

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -9,13 +9,13 @@ class IndexController extends Action {
 	public function index() {
 
 		$this->view->dados = array('Sofá', 'Cadeira', 'Cama');
-		$this->render('index');
+		$this->render('index', 'layout1');
 	}
 
 	public function sobreNos() {
 		
 		$this->view->dados = array('Notebook', 'Smartphone');
-		$this->render('sobreNos');
+		$this->render('sobreNos', 'layout2');
 	}
 
 }
